f20ce9c1b483f0b19fe5bee60a94b5717f53fb5b
Rudolph Pienaar <rudolph.pienaar@childrens.harvard.edu>
2017-10-17 21:18:07 -0400

    More convolutions

--- pfmisc/__init__.py
+++ pfmisc/__init__.py
@@ -1,5 +1,12 @@
-from ._colors   import Colors
-from .debug     import debug
-from .message   import Message
-from .pfmisc    import pfmisc
-from .pfmisc2   import pfmisc2
+try:
+    from ._colors   import Colors
+    from .debug     import debug
+    from .message   import Message
+    from .pfmisc    import pfmisc
+    from .pfmisc2   import pfmisc2
+except:
+    from _colors   import Colors
+    from debug     import debug
+    from message   import Message
+    from pfmisc    import pfmisc
+    from pfmisc2   import pfmisc2
