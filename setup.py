f9685bc7772e444d47c30bd0e9105a68efe27712
Rudolph Pienaar <rudolph.pienaar@childrens.harvard.edu>
2017-10-18 08:49:51 -0400

    v0.99.12

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ def readme():
 
 setup(
       name             =   'pfmisc',
-      version          =   '0.99.11',
+      version          =   '0.99.12',
       description      =   'Misc utilities for FNNDSC python repos',
       long_description =   readme(),
       author           =   'Rudolph Pienaar',
