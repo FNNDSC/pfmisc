d022e3f70751b9253c7691878726af3e89b7e33d
Rudolph Pienaar <rudolph.pienaar@gmail.com>
2018-07-10 16:55:08 -0400

    v1.3.0

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ def readme():
 
 setup(
       name             =   'pfmisc',
-      version          =   '1.2.2',
+      version          =   '1.3.0',
       description      =   'Misc utilities for FNNDSC python repos',
       long_description =   readme(),
       author           =   'Rudolph Pienaar',
