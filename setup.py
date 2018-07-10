e572d10e8f821ef93465918bd4f242628fe27793
Rudolph Pienaar <rudolph.pienaar@gmail.com>
2018-07-10 17:15:25 -0400

    Update to 1.3.2

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ def readme():
 
 setup(
       name             =   'pfmisc',
-      version          =   '1.3.0',
+      version          =   '1.3.2',
       description      =   'Misc utilities for FNNDSC python repos',
       long_description =   readme(),
       author           =   'Rudolph Pienaar',
