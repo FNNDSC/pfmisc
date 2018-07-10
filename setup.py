bd51c30656bb7d33e4288f44b39f06f89b026666
Rudolph Pienaar <rudolph.pienaar@gmail.com>
2018-07-10 17:23:24 -0400

    Bump to 1.3.4

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ def readme():
 
 setup(
       name             =   'pfmisc',
-      version          =   '1.3.2',
+      version          =   '1.3.4',
       description      =   'Misc utilities for FNNDSC python repos',
       long_description =   readme(),
       author           =   'Rudolph Pienaar',
